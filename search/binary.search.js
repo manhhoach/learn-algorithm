d35b0741f432a8b94bff113d9ac762062bb59ca1
manhhoach <manhhn.nv5@gmail.com>
2023-05-13 21:14:25 +0700

    binary search

--- search/binary.search.js
+++ search/binary.search.js
@@ -1,1 +1,11 @@
-// implement here
+const a = [1,5,8,4,6,12,45,11];
+const i=6;
+
+
+const binarySearch = (arr, item)=>{
+    // implement here
+
+    return item
+}
+
+console.log(`Value to look for at ${binarySearch(a, i)}`)
